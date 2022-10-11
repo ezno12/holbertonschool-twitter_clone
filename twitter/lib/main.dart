5fd9808b2ea59fc585e1f865017471096a782609
tennin12 <bassembenyhia12@gmail.com>
2022-10-11 18:06:41 +0100

    add multi provider, sharedstate added to change notifier provider, and my app change to child

--- twitter/lib/main.dart
+++ twitter/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:twitter/providers/share_state.dart';
 import 'package:twitter/screens/home_screen.dart';
 import 'package:twitter/screens/signin_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => SharedState())
+    ],
+    // ignore: prefer_const_constructors
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
